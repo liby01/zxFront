e7ac14375a69b29d9f0ae40525992a64e97b6692
liby <Liboyang19965263@gmail.com>
2024-06-22 16:31:00 +0800

    分类管理
    分类管理

---
+++ zx-admin/zx-admin/src/api/category.js
@@ -0,0 +1,11 @@
+import request from '@/utils/request'
+
+const api_name = '/admin/product/category'
+
+// 根据parentId获取下级节点
+export const FindCategoryByParentId = parentId => {
+  return request({
+    url: `${api_name}/findCategoryList/${parentId}`,
+    method: 'get',
+  })
+}
